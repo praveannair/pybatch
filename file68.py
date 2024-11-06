fa8e31dc477dedaeb4d33a213914fde6b0af496f
praveen <nairxprv@gmail.com>
2024-11-06 18:29:33 +0530

    added mysql

---
+++ file68.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root",
+  database="myappdb"
+)
+mycursor = mydb.cursor()
+
+while True:
+    name = input("Enter name: ")
+    email = input("Enter email: ")
+
+    sql = "INSERT INTO customers (name, email) VALUES (%s, %s)"
+    val = (name,email)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+    ch = input("Continue?(y/n)")
+    if ch!='y':
+        break
+    
+mycursor.execute("select * from customers")
+myresult = mycursor.fetchall()
+for x in myresult:
+    print(x)
